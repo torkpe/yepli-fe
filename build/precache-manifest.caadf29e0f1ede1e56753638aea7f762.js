1132a7e99afa084a69217af615b555bb7220eccb
temitope emmanuel <temitope.emmanuel63@gmail.com>
2020-05-13 09:19:34 +0100

    add build

---
+++ build/precache-manifest.caadf29e0f1ede1e56753638aea7f762.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff495c78ebe1bb5a1b4115066c8ffa9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77a7865fae5a8cc960e8",
+    "url": "/static/css/main.b517175e.chunk.css"
+  },
+  {
+    "revision": "f3eeee777c46d19d6978",
+    "url": "/static/js/2.f430acb7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f430acb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77a7865fae5a8cc960e8",
+    "url": "/static/js/main.1b963ea5.chunk.js"
+  },
+  {
+    "revision": "33df72e9c7df461aa736",
+    "url": "/static/js/runtime-main.c44b705a.js"
+  },
+  {
+    "revision": "b2dd69df9dee1d6c5b606d455d287dbd",
+    "url": "/static/media/Deals.b2dd69df.svg"
+  },
+  {
+    "revision": "cf8c61a719c57885ce595a1d4d8ffe66",
+    "url": "/static/media/My-deals.cf8c61a7.svg"
+  },
+  {
+    "revision": "18b22d9bdf5dc972b2c70edcfdccac08",
+    "url": "/static/media/Settings.18b22d9b.svg"
+  },
+  {
+    "revision": "9c5cd32498a6ef19ccdf13c5d2ec0352",
+    "url": "/static/media/Task.9c5cd324.svg"
+  },
+  {
+    "revision": "73dbedef7656cfbe7fdc09c13a26f978",
+    "url": "/static/media/Templates.73dbedef.svg"
+  },
+  {
+    "revision": "104a90a2964e4e4c4c8f23cec50a87a0",
+    "url": "/static/media/contact.104a90a2.svg"
+  },
+  {
+    "revision": "f9515263fdc5815c71126940f72cd56f",
+    "url": "/static/media/image1.f9515263.png"
+  },
+  {
+    "revision": "67b5a77293a5cd91779f6186f85d6dac",
+    "url": "/static/media/lock.67b5a772.svg"
+  },
+  {
+    "revision": "b9f0f57cefd34dc0ec08c58810881dc7",
+    "url": "/static/media/mail.b9f0f57c.svg"
+  },
+  {
+    "revision": "2662449a4884490f19d4020efe5963fc",
+    "url": "/static/media/my-task.2662449a.svg"
+  },
+  {
+    "revision": "1762900702f3b5b96d806be2128e3aaa",
+    "url": "/static/media/user.17629007.svg"
+  }
+]);
